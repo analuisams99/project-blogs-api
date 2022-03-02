43134d4980c956863db419e2e5252d86bdb22d9c
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-03-01 21:34:56 -0300

    Cria rota para listar todos os posts existentes

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { create } = require('../controllers/post');
+const { create, getAll } = require('../controllers/post');
 const { validateBlogPost } = require('../middlewares/blogPostValidation');
 const { validateToken } = require('../middlewares/tokenValidation');
 
 const router = express.Router();
 
 router
-  .post('/', validateToken, validateBlogPost, create);
+  .post('/', validateToken, validateBlogPost, create)
+  .get('/', validateToken, getAll);
 
 module.exports = router;
